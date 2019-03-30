2eddf5b39f0349420d1260118269a40ac2882454
Matthew-Ruben <36095451+Matthew-Ruben@users.noreply.github.com>
2019-03-29 21:03:34 -0700

    hw8_2.py -> hw8.py & game_2.py -> game.py

--- game.py
+++ game.py
@@ -3,7 +3,6 @@ import random
 
 
 class ZombieShooter:
-
     """
     class to support a GUI with animated images.
 
@@ -19,7 +18,6 @@ class ZombieShooter:
     old_car: (integer) object ID of the old car image created on canvas.
     """
 
-
     def __init__(self, parent):
         parent.title('Lets play the game!')
         self.parent = parent
@@ -27,24 +25,21 @@ class ZombieShooter:
                                       command=self.start)
         start_button.grid()  # register it with a geometry manager
 
-
         self.sammy_image = tkinter.PhotoImage(file='sammy.gif')
         status = tkinter.Label(parent, text='Ready to Start')
         status.grid()
 
         self.canvas = tkinter.Canvas(parent, width=400, height=400,
-                                      background='blue')
-        self.canvas.create_image(200,200,image = self.sammy_image)
+                                     background='blue')
+        self.canvas.create_image(200, 200, image=self.sammy_image)
         self.sammy = self.canvas.create_rectangle(0, 200, 25, 250,
                                                   fill='yellow')
-        self.zombies=[]
-        self.bullet =[]
+        self.zombies = []
+        self.bullets = []
         self.canvas.bind_all('<Key>', self.move)
-        self.go=False
+        self.go = False
         self.shooting()
         self.canvas.grid()
-
-
 
     def start(self):
         """
@@ -52,46 +47,58 @@ class ZombieShooter:
         :return: None
         """
         for i in range(5):
-            ztx=random.randint(400,600)
-            zty=random.randint(0,375)
+            ztx = random.randint(400, 600)
+            zty = random.randint(0, 375)
             self.zombies.append(self.canvas.create_rectangle(ztx, zty,
-                                                          ztx+25,zty+25,
-                                                          fill='black'))
+                                                             ztx + 25,
+                                                             zty + 25,
+                                                             fill='black'))
         self.go = True
         self.animate()
+
     def restart(self):
         pass
 
-
-    def move(self,event):
-        xt, yt, xb, yb = self.canvas.coords(self.sammy)
+    def move(self, event):
+        sammy_left, \
+            sammy_top, \
+            sammy_right, \
+            sammy_bottom = self.canvas.coords(self.sammy)
         if event.keysym == 'w':
-            if yt > 0:
-              self.canvas.move(self.sammy, 0, -25)
+            if sammy_top > 0:
+                self.canvas.move(self.sammy, 0, -25)
         if event.keysym == 's':
-            if yb < 400:
-              self.canvas.move(self.sammy, 0, 25)
+            if sammy_bottom < 400:
+                self.canvas.move(self.sammy, 0, 25)
         if event.keysym == 'j':
-            self.bullet.append(self.canvas.create_oval(25, yt+20,
-                                              35, yt + 30, fill='red'))
-            self.go= True
-
+            self.bullets.append(self.canvas.create_oval(25, sammy_top + 20,
+                                                        35, sammy_top + 30,
+                                                        fill='red'))
+            self.go = True
 
     def shooting(self):
         if self.go:
-            for b in self.bullet:
-                bxt, byt, bxb, byb = self.canvas.coords(b)
+            for b in self.bullets:
+                bullet_left, \
+                    bullet_top, \
+                    bullet_right, \
+                    bullet_bottom = self.canvas.coords(b)
                 for z in self.zombies:
-                    xt, yt, xb, yb = self.canvas.coords(z)
-                    if byt <= yb and byb >= yt and bxb == xt:
+                    zombie_left, \
+                        zombie_top, \
+                        zombie_right, \
+                        zombie_bottom = self.canvas.coords(z)
+                    if bullet_top <= zombie_bottom \
+                            and bullet_bottom >= zombie_top \
+                            and bullet_right == zombie_left:
                         self.zombies.remove(z)
                         self.canvas.delete(z)
-                        self.bullet.remove(b)
+                        self.bullets.remove(b)
                         self.canvas.delete(b)
                     else:
                         self.canvas.move(b, 1, 0)
 
-                #self.check_collision(b)
+                # self.check_collision(b)
 
         self.parent.after(10, self.shooting)
 
@@ -105,15 +112,13 @@ class ZombieShooter:
     #             self.canvas.move(z,0, 500)
     #             self.canvas.move(b, 0, 500)
 
-
     def animate(self):
         if self.go:
             for z in self.zombies:
-                xt,yt,xb,yb = self.canvas.coords(z)
-                if xt>0:
+                xt, yt, xb, yb = self.canvas.coords(z)
+                if xt > 0:
                     self.canvas.move(z, -1, 0)
         self.parent.after(10, self.animate)
-
 
 
 def main():
--- hw8.py
+++ hw8.py
@@ -1,8 +1,8 @@
 # ----------------------------------------------------------------------
-# Name:        nascar0
-# Purpose:     demonstrate animation with tkinter
+# Name:        hw8_2
+# Purpose:     Create animated video game
 #
-# Author:      Rula Khayrallah
+# Author:      Yiyi Zhang & Matthew Ruben
 # ----------------------------------------------------------------------
 """
 Implement a GUI app with animation in tkinter.
@@ -11,79 +11,188 @@ Create then animate two car images in response to a START and STOP
 buttons.
 """
 import tkinter
-import zombie
+import random
+import argparse
+
+
+def get_arguments():
+    # Argument Parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num_zombies',
+                        help='How many zombies you would like to face',
+                        default=20)
+    parser.add_argument('player_name',
+                        help='What is your name',
+                        default='Spartan Sammy')
+    arguments = parser.parse_args()
+    num_zombies = arguments.num_zombies
+    player_name = arguments.player_name
+    print(f'num_zombies = {repr(num_zombies)}')
+    print(f'player_name = {repr(player_name)}')
+    return int(num_zombies), player_name
 
 
 class ZombieShooter:
+    y_top_margin = 25
+    y_bottom_margin = 15
+    window_height = 400
+    window_width = 600
+    bg_color = 'white'
+    sammy_image = 'sammy.gif'
+    zombie_image = 'zombie.gif'
+    window_title = 'Lets play the game!'
 
-    """
-    class to support a GUI with animated images.
+    sammy_width = 50
+    sammy_height = 50
+    sammy_offset = sammy_width / 2
+    zombie_width = 50
+    zombie_height = 50
+    zombie_speed = 2
+    zombie_offset = zombie_width / 2
+    bullet_width = 15
+    bullet_height = 15
+    bullet_speed = 2
 
-    Argument:
-    parent: (tkinter.Tk) the root window object
-
-    Attributes:
-    parent: (tkinter.Tk) the root window object
-    canvas: (tkinter.Canvas) A Canvas widget defining the race area.
-    red_car_image: (tkinter.PhotoImage) image of a red car.
-    old_car_image: (tkinter.PhotoImage) image of an old car.
-    red_car: (integer) object ID of the red car image created on canvas.
-    old_car: (integer) object ID of the old car image created on canvas.
-    """
+    sammy_step = 10
 
     def __init__(self, parent):
-        parent.title('Lets play the game!')
+        # Utilize command line arguments
+        self.num_zombies, self.player_name = get_arguments()
+
         self.parent = parent
+        self.window_height = ZombieShooter.window_height
+        self.window_width = ZombieShooter.window_width
+        self.bg_color = ZombieShooter.bg_color
 
-        # create a START button and associate it with the start method
-        start_button = tkinter.Button(parent, text='START', width=20,
-                                      command=self.start)
+        self.canvas = tkinter.Canvas(parent,
+                                     width=self.window_width,
+                                     height=self.window_height,
+                                     background=self.bg_color)
 
-        start_button.grid()  # register it with a geometry manager
-
-        # # create a STOP button and associate it with the stop method
-        # stop_button = tkinter.Button(parent, text='STOP', width=20,
-        #                              command=self.stop)
-        # stop_button.grid() # register it with a geometry manager
-        # # create a Canvas widget for the animated objects
-
-        # self.sammy_image = tkinter.PhotoImage(file='sammy1.jpg')
-        self.zombie_image = tkinter.PhotoImage(file=zombie.Zombie.get_image_loc())
-        status = tkinter.Label(parent, text='Ready to Start')
-        status.grid()
-
-        self.canvas = tkinter.Canvas(parent, width=500, height=500,
-                                     background='blue')
+        self.parent.title(ZombieShooter.window_title)
 
         # Sprite Collections
+        self.zombie_images = []
         self.zombies = []
-        self.sammy = []
-        self.bullets = {}
+        self.bullets = []
+
+        # Create Sammy
+        self.sammy_img = tkinter.PhotoImage(file=ZombieShooter.sammy_image)
+        self.sammy = self.canvas.create_image(50, 300, image=self.sammy_img)
+
+        # Create Zombies
+        for i in range(self.num_zombies):
+            self.zombie_images.insert(i, tkinter.PhotoImage(
+                file=ZombieShooter.zombie_image))
+            self.zombies.insert(i, self.canvas.create_image(
+                self.get_start_x(),
+                self.get_start_y(),
+                image=self.zombie_images[i]))
+        print(str(len(self.zombies)) + " zombies spawned")
+        self.check_collisions()
+
+        # Create Start button
+        start_button = tkinter.Button(parent,
+                                      text="START",
+                                      width=20,
+                                      command=self.start)
+        start_button.grid()
+
+        self.canvas.bind_all('<Key>', self.handle_key_presses)
+        self.canvas.bind_all('<space>', self.shoot)
+        self.canvas.bind('<Button-1>', self.mouse_clicked)
 
         self.go = False
-
+        self.shooting()
         self.canvas.grid()
 
+    def mouse_clicked(self, event):
+        print(f'click at (x:{event.x}, y:{event.y})')
+
     def start(self):
-        """
-        This method is invoked when the user presses the START button
-        :return: None
-        """
-        self.zombies = self.canvas.create_image(25, 50,
-                                                image=Zombie.get_image)
+        print("Start button pressed: Let the zombie horde out!")
+        # returns early if go is already true
+        # this prevents unwanted behavior of animated objects
+        if self.go:
+            return
         self.go = True
+
+        # Start Zombies
+        for zombie in self.zombies:
+            self.canvas.move(zombie, ZombieShooter.zombie_speed, 0)
+
         self.animate()
 
-    def add(self):
-        """
-        This method is invoked when the user presses the STOP button
-        :return: None
-        """
-        self.go = False
-        self.animate()
+    def handle_key_presses(self, event):
+        x, y = self.canvas.coords(self.sammy)
+        if event.keysym == 'w':
+            if y > ZombieShooter.y_top_margin:
+                self.canvas.move(self.sammy, 0, -1 * ZombieShooter.sammy_step)
+        if event.keysym == 's':
+            if y < ZombieShooter.window_height - ZombieShooter.y_bottom_margin:
+                self.canvas.move(self.sammy, 0, ZombieShooter.sammy_step)
+        if event.keysym == 'j':
+            print('j')
+
+    def shoot(self, event):
+        sammy_x, sammy_y = self.canvas.coords(self.sammy)
+        sammy_x_offset = ZombieShooter.sammy_width / 2
+        sammy_y_offset = ZombieShooter.sammy_height / 2
+        x_offset = 5
+        y_offset = -20
+
+        self.bullets.append(self.canvas.create_oval(
+            sammy_x + x_offset + sammy_x_offset,  # x0
+            sammy_y + y_offset + sammy_y_offset,  # y0
+            sammy_x + x_offset + ZombieShooter.bullet_width,  # x1
+            sammy_y + y_offset + ZombieShooter.bullet_height,  # y1
+            fill='red')
+        )
+
+    def shooting(self):
+
+        if self.go:
+            for b in self.bullets:
+                self.canvas.move(b, ZombieShooter.bullet_speed, 0)
+        self.parent.after(10, self.shooting)
+
+    def get_start_y(self):
+        return random.randrange(ZombieShooter.y_top_margin,
+                                self.window_height,
+                                25)
+
+    def get_start_x(self):
+        return self.window_width + random.randrange(1, self.window_width, 25)
 
     def animate(self):
-        pass
+        for zombie in self.zombies:
+            if self.go:
+                x, y = self.canvas.coords(zombie)
+                if x > 50:
+                    self.canvas.move(zombie, -2, 0)
+
+        self.parent.after(10, self.animate)  # Try again in 1ms
+
+    def check_collisions(self):
+        for z in self.zombies:
+            zombie_mid_x, zombie_mid_y = self.canvas.coords(z)
+            print(zombie_mid_x)
+            print(zombie_mid_y)
+
+
+class Zombie:
+    picture = 'zombie.gif'
+
+    def __init__(self, canvas, x=500, y=300, hp=100, speed=10):
+        self.hp = hp
+        self.speed = speed
+        self.canvas = canvas
+
+        self.photo_image = tkinter.PhotoImage(file=Zombie.picture)
+        self.canvas_item = self.canvas.create_image(x,
+                                                    y,
+                                                    image=self.photo_image)
+        self.canvas.grid()
 
 
 def main():
